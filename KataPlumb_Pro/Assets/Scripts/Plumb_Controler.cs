0953ce812c34cf1a3f13d082437051eeee236c41
AmberZxxiv <amberzxxiv@gmail.com>
2025-10-22 20:07:32 +0200

    Materials y Pumb control
    añadimos materiales, main menu y las tuberías funcionales

---
+++ KataPlumb_Pro/Assets/Scripts/Plumb_Controler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plumb_Controler : MonoBehaviour
+{
+    public bool isBrocken;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

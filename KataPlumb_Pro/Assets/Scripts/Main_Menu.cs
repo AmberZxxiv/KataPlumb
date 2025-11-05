e222b859fb9c897f89beab62e2db3a64dd9b4860
AmberZxxiv <amberzxxiv@gmail.com>
2025-11-05 20:26:45 +0100

    moar plumbs

--- KataPlumb_Pro/Assets/Scripts/Main_Menu.cs
+++ KataPlumb_Pro/Assets/Scripts/Main_Menu.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Main_Menu : MonoBehaviour
 {
+    public Player_Control _PC;
+    public TextMeshProUGUI maxscore;
+
     // Start is called before the first frame update
     void Start()
     {
+        _PC = Player_Control.instance;
         Time.timeScale = 1;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        maxscore.text = "Employee of the Game: " + PlayerPrefs.GetInt("MaxScore").ToString() + " $";
     }
 
     // Update is called once per frame
